e1e9a563aee4bb2bb8126a160dbf35baca763a56
Gonzalo Castiglione <gcastigl@gmail.com>
2012-03-10 18:49:17 -0300

    create da mini board (2x2)

--- TPE_pre1/src/edificios/BuildingParser.java
+++ TPE_pre1/src/edificios/BuildingParser.java
@@ -4,10 +4,24 @@ package edificios;
 public class BuildingParser {
 	
 	public static Board parse(String filename){
+		return board1();
+	}
+
+	private static Board board1() {
 		int[][] rules = {{1,2,2},{3,1,2},{1,2,2}, {3,1,2}};
 		Settings.restrictions = rules;
 		Board parsed = new Board(rules[0].length);
 		return parsed;
 	}
-
+	
+	private static Board board2() {
+		int[][] rules = new int[4][];
+		rules[Settings.TOP] = new int[] {2, 1};
+		rules[Settings.BOTTOM] = new int[] {1, 2};
+		rules[Settings.LEFT] = new int[] {2, 1};
+		rules[Settings.RIGHT] = new int[] {1, 2};
+		Settings.restrictions = rules;
+		Board parsed = new Board(rules[0].length);
+		return parsed;
+	}
 }

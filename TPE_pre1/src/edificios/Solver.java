a7646c77cfaf476d4f40fbd1912d26df1422a310
Gonzalo Castiglione <gcastigl@gmail.com>
2012-03-10 15:53:44 -0300

    deleted "if" statements checking what kind of strategy will be used.

--- TPE_pre1/src/edificios/Solver.java
+++ TPE_pre1/src/edificios/Solver.java
@@ -1,35 +1,45 @@
 package edificios;
 
-import util.Logger;
 import gps.SearchStrategy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import util.Logger;
 
 
 public class Solver {
 
 	public static void main(String[] args) {
-		args = new String[] {"DFS"};
 		if(args.length == 0){
 			System.out.println("Please declare what searching method you want to use (BFS, DFS)");
 			System.out.println("Next time run: java -jar Solver.java BFS");
 			return;
 		}
-
+		// init logger
 		Logger.init();
 		Logger.LOG_LEVEL = Logger.LEVEL_TRACE;
 		
+		Map<String, SearchStrategy> startegies = getStreategies();
+		SearchStrategy se = startegies.get(args[0]);
+		if (se == null) {
+			Logger.log("Solver", args[0] + "strategy not found", Logger.LEVEL_ERROR);
+			return;
+		}
+		// init problem engine solver
 		BuildingProblem prob = new BuildingProblem();
 		BuildingEngine eng = new BuildingEngine();
 		long initialTime = System.currentTimeMillis();
-		if(args[0].equals("DFS")){
-			eng.engine(prob, SearchStrategy.DFS);
-		} else if(args[0].equals("BFS")){
-			eng.engine(prob, SearchStrategy.BFS);
-		} else{
-			Logger.log("Solver", args[0] + "strategy not found", Logger.LEVEL_TRACE);
-			return ;
-		}
+		eng.engine(prob, se);
 		long elapsedTime = System.currentTimeMillis() - initialTime;
 		Logger.log("Result", "Algorithm took " + elapsedTime + " ms", Logger.LEVEL_TRACE);
 		
+	}
+	
+	private static Map<String, SearchStrategy> getStreategies() {
+		Map<String, SearchStrategy> startegy = new HashMap<String, SearchStrategy>();
+		startegy.put("DFS", SearchStrategy.DFS);
+		startegy.put("BFS", SearchStrategy.BFS);
+		return startegy;
 	}
 }

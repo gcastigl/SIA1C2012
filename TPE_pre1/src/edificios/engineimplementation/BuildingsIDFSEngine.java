577549ae666816432a9cbb2d35f0b8fcdfe846f2
Gonzalo Castiglione <gcastigl@gmail.com>
2012-03-11 15:01:07 -0300

    for the IDFS, if the first rule to be processes was first, then that state was evaluated last, so it was accually creating the worst case for this algorithm

--- TPE_pre1/src/edificios/engineimplementation/BuildingsIDFSEngine.java
+++ TPE_pre1/src/edificios/engineimplementation/BuildingsIDFSEngine.java
@@ -9,21 +9,33 @@ import java.util.LinkedList;
 public class BuildingsIDFSEngine extends GPSEngine {
 
 	private int currentMaxDepth = 1;
+	private int index = 0, indexDepth = 0;
 	
 	@Override
 	public void addNode(GPSNode node) {
-		((LinkedList<GPSNode>) open).addFirst(node);
+		if (indexDepth != node.getDepth()) {
+			indexDepth = node.getDepth();
+			index = 0;
+		}
+		((LinkedList<GPSNode>) open).add(index, node);
+		index++;
 	}
 	
 	@Override
 	protected boolean explode(GPSNode node) {
 		if (node.getDepth() >= currentMaxDepth) {
-			open.clear();
+			resetToInitialState();
 			open.add(new GPSNode(problem.getInitState(), 0));
 			currentMaxDepth++;
 			return true;
 		}
 		return super.explode(node);
+	}
+	
+	private void resetToInitialState() {
+		index = 0;
+		indexDepth = 0;
+		open.clear();
 	}
 	
 	@Override

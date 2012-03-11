9cba98fabb6de81dda75a32f14d7621b467ec4f7
Gonzalo Castiglione <gcastigl@gmail.com>
2012-03-11 17:05:20 -0300

    if a number n is found at some restriction, then all rules to put 1 to n building in order are put first in the list.

--- TPE_pre1/src/edificios/BuildingProblem.java
+++ TPE_pre1/src/edificios/BuildingProblem.java
@@ -43,6 +43,10 @@ public class BuildingProblem implements GPSProblem {
 		for (int i = 0; i < topRules.length; i++) {
 			if (topRules[i] == 1) {
 				setRuleFirst(0, i, topHeigth);
+			} else if (topRules[i] == topHeigth) {
+				for (int j = 0; j < topHeigth; j++) {
+					setRuleFirst(j, i, j + 1);
+				}
 			}
 		}
 	}
@@ -53,6 +57,10 @@ public class BuildingProblem implements GPSProblem {
 		for (int i = 0; i < bottomRules.length; i++) {
 			if (bottomRules[i] == 1) {
 				setRuleFirst(topHeigth - 1, i, topHeigth);
+			} else if (bottomRules[i] == topHeigth) {
+				for (int j = topHeigth; j > 0; j--) {
+					setRuleFirst(j - 1, i, topHeigth - j + 1);
+				}
 			}
 		}
 	}
@@ -63,6 +71,10 @@ public class BuildingProblem implements GPSProblem {
 		for (int i = 0; i < leftRules.length; i++) {
 			if (leftRules[i] == 1) {
 				setRuleFirst(i, 0, topHeigth);
+			} else if (leftRules[i] == topHeigth) {
+				for (int j = 0; j < topHeigth; j++) {
+					setRuleFirst(i, j, j + 1);
+				}
 			}
 		}
 	}
@@ -73,6 +85,10 @@ public class BuildingProblem implements GPSProblem {
 		for (int i = 0; i < rightRules.length; i++) {
 			if (rightRules[i] == 1) {
 				setRuleFirst(i, topHeigth - 1, topHeigth);
+			} else if (rightRules[i] == topHeigth) {
+				for (int j = topHeigth; j > 0; j--) {
+					setRuleFirst(i, j - 1, topHeigth - j + 1);
+				}
 			}
 		}
 	}

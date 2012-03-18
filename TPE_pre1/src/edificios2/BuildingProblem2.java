5ad158c54e738c89111b9c67bc00797b3bbc85d6
Gonzalo Castiglione <gcastigl@gmail.com>
2012-03-18 20:28:58 -0300

    implmeneted heuristic numer 2

--- TPE_pre1/src/edificios2/BuildingProblem2.java
+++ TPE_pre1/src/edificios2/BuildingProblem2.java
@@ -28,9 +28,72 @@ public class BuildingProblem2 extends BuildingProblem {
 	@Override
 	public Integer getHValue(GPSState state) {
 		Board board = ((BuildingState) state).getCurrentBoard();
-		// TODO: terminar esta funcion
+		if (checkNoSolution(board)) {
+			return Integer.MAX_VALUE;
+		}
 		int n = board.getSize();
 		return n * n - board.getBuildingOnBoard();
 	}
 
+	private boolean checkNoSolution(Board board) {
+		int[][] buildings = board.getBuildings();
+		int n = board.getSize();
+		// check top restrictions
+		for (int i = 0; i < n; i++) {
+			int a = Settings.restrictions[Settings.TOP][i];
+			for (int row = 0; row < n; row++) {
+				int k = buildings[row][i];
+				if (k == 0) {
+					continue;
+				}
+				int minDist = (a + k) - n - 1;
+				if (row < minDist) {
+					return true;
+				}
+			}
+		}
+		// check bottom restrictions
+		for (int i = 0; i < n; i++) {
+			int a = Settings.restrictions[Settings.BOTTOM][i];
+			for (int row = 0; row < n; row++) {
+				int k = buildings[row][i];
+				if (k == 0) {
+					continue;
+				}
+				int minDist = (a + k) - n - 1;
+				if ((n - row - 1) < minDist) {
+					return true;
+				}
+			}
+		}
+		// check left restrictions
+		for (int i = 0; i < n; i++) {
+			int a = Settings.restrictions[Settings.LEFT][i];
+			for (int col = 0; col < n; col++) {
+				int k = buildings[i][col];
+				if (k == 0) {
+					continue;
+				}
+				int minDist = (a + k) - n - 1;
+				if (col < minDist) {
+					return true;
+				}
+			}
+		}
+		// check right restrictions
+		for (int i = 0; i < n; i++) {
+			int a = Settings.restrictions[Settings.RIGHT][i];
+			for (int col = 0; col < n; col++) {
+				int k = buildings[i][col];
+				if (k == 0) {
+					continue;
+				}
+				int minDist = (a + k) - n - 1;
+				if ((n - col - 1) < minDist) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }

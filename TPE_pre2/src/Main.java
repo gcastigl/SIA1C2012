4b68ff9fbfdc5d9d642766a5da2228ab7a88784f
Gonzalo Castiglione <gcastigl@gmail.com>
2012-04-03 19:06:19 -0300

    created a test method

--- TPE_pre2/src/Main.java
+++ TPE_pre2/src/Main.java
@@ -1,14 +1,14 @@
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class Main {
 
 	public static void main(String[] args) {
 		SimplePerceptron perceptron = new SimplePerceptron(2, 1);
 		trainAnd(perceptron);
-		float[] eval = perceptron.evaluate(new float[] { 1, 1 });
-		System.out.println(Arrays.toString(eval));
+		testAnd(perceptron);
 	}
 
 	private static void trainAnd(SimplePerceptron perceptron) {
@@ -20,5 +20,20 @@ public class Main {
 		trainng.put(new float[] { 0, 1 }, new float[] { 0 });
 		perceptron.train(trainng, 100);
 		System.out.println("After training => " + perceptron);
+	}
+	
+	private static void testAnd(SimplePerceptron perceptron) {
+		Map<float[], float[]> test = new HashMap<float[], float[]>();
+		test.put(new float[] { 1, 1 }, new float[] { 1 });
+		test.put(new float[] { 0, 0 }, new float[] { 0 });
+		test.put(new float[] { 1, 0 }, new float[] { 0 });
+		test.put(new float[] { 0, 1 }, new float[] { 0 });
+		System.out.println("\nTesting perceptron: " + perceptron);
+		for (Entry<float[], float[]> testEntry: test.entrySet()) {			
+			float[] eval = perceptron.evaluate(testEntry.getKey());
+			System.out.println(Arrays.toString(testEntry.getKey()) + 
+				" - test passed? " + Arrays.equals(eval, testEntry.getValue()));
+		}
+		System.out.println("-------------------------");
 	}
 }

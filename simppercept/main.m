ca84d0e67455a40c43219d8fe722956d6974bc0b
martin <martin@martin-Inspiron-1545.(none)>
2012-04-05 18:42:33 -0300

    Hermoso!

--- simppercept/main.m
+++ simppercept/main.m
@@ -1,5 +1,5 @@
 function net = main(trans)
-	epochs = 200;
+	epochs = 1000;
 	i1 = { [-1,-1,-1,1] , 1};
 	i2 = { [-1,-1,-1,-1] , -1};
 	i3 = {[1,1,1,1], 1};
@@ -9,12 +9,16 @@ function net = main(trans)
 	i7 = {[-1,1,-1,-1] , 1};
 	i8 = {[-1,-1,1,-1] , 1};
 	vals = {i1,i2,i3,i4,i5,i6,i7,i8};
+	train_set = 8;
 	net = crt_nr_nw(4,1,1,trans);
+	errors = zeros(epochs * train_set,1);
 	for i = 1:epochs
-		vec = get_randorder(8);
-		for j = 1:8
+		vec = get_randorder(train_set);
+		for j = 1:train_set
 			net = eval_input(net, vals{vec(j)}{1});
 			net = update_weights(net, vals{vec(j)}{2});
+			errors(i * train_set + j) = get_error(net, vals{vec(j)}{2});
 		end
 	end
+	plot(errors);
 endfunction

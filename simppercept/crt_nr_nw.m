9dceeb8d2e24b672b3c3e797573738485e9ffb59
martin <martin@martin-Inspiron-1545.(none)>
2012-04-05 19:29:29 -0300

    Implementado annealed y smart learn rate

--- simppercept/crt_nr_nw.m
+++ simppercept/crt_nr_nw.m
@@ -1,4 +1,4 @@
-function net = crt_nr_nw( in_len, out_len, lrn_type, trans_type)
+function net = crt_nr_nw( in_len, out_len, lrn_type, base_lrn_rate, trans_type)
 	%create a new simple perceptron.
 	% Requieres: input length, output length, learn type (static, annealed or dynamic),
 	% and cost_type (threshold, linear or sigmoid)
@@ -7,9 +7,11 @@ function net = crt_nr_nw( in_len, out_len, lrn_type, trans_type)
 	net.values{1}(in_len+1) = -1;
 	net.lrn_type = lrn_type;
 	net.trans_type = trans_type;
-	net.lrn_rt = 0.02;
+	net.lrn_rt = base_lrn_rate;
 	net.lrn_decay = 0.85;
 	net.lrn_consist = 3;
 	net.lrn_sum = 0.005;
+	net.counter = 0;
+	net.prev_err = 99999999;
 	net.beta = 1;
 endfunction
--- simppercept/main.m
+++ simppercept/main.m
@@ -1,24 +1,42 @@
-function net = main(trans)
-	epochs = 1000;
-	i1 = { [-1,-1,-1,1] , 1};
-	i2 = { [-1,-1,-1,-1] , -1};
-	i3 = {[1,1,1,1], 1};
-	i4 = {[-1,1,-1,1],1};
-	i5 = {[1,1,-1,-1] , 1};
-	i6 = {[1,-1,-1,-1], 1};
-	i7 = {[-1,1,-1,-1] , 1};
-	i8 = {[-1,-1,1,-1] , 1};
-	vals = {i1,i2,i3,i4,i5,i6,i7,i8};
+function net = main(trans, lrn_base, lrn_type)
+	epochs = 500;
+	t1 = { [-1,-1,-1,1] , 1};
+	t2 = { [-1,-1,-1,-1] , -1};
+	t3 = {[1,1,1,1], 1};
+	t4 = {[-1,1,-1,1],1};
+	t5 = {[1,1,-1,-1] , 1};
+	t6 = {[1,-1,-1,-1], 1};
+	t7 = {[-1,1,-1,-1] , 1};
+	t8 = {[-1,-1,1,-1] , 1};
+	vals = {t1,t2,t3,t4,t5,t6,t7,t8};
+	i1 = {[-1,-1,-1,1], 1};
+	i2 = {[1, -1, 1, 1],1};
+	i3 = {[-1,-1,1,1],1};
+	i4 = {[-1,1,1,-1],1};
+	test_set = 4;
+	tests = {i1,i2,i3,i4};
 	train_set = 8;
-	net = crt_nr_nw(4,1,1,trans);
-	errors = zeros(epochs * train_set,1);
+	net = crt_nr_nw(4,1,lrn_type,lrn_base,trans);
+	errors = zeros(epochs,1);
+	test_errors = zeros(epochs,1);
+	lrn_rates = zeros(epochs,1);
 	for i = 1:epochs
 		vec = get_randorder(train_set);
+		err = 0;
 		for j = 1:train_set
 			net = eval_input(net, vals{vec(j)}{1});
 			net = update_weights(net, vals{vec(j)}{2});
-			errors(i * train_set + j) = get_error(net, vals{vec(j)}{2});
+			err += get_error(net, vals{vec(j)}{2});
 		end
+		errors(i) = err / train_set;
+		terror = 0;
+		for  j = 1:test_set
+			net = eval_input(net, tests{j}{1});
+			terror += get_error(net, tests{j}{2});
+		end
+		test_errors(i) = terror;
+		net = update_lrn_rate(net, errors(i));
+		lrn_rates(i) = net.lrn_rt;
 	end
-	plot(errors);
+	plot(1:epochs, errors, 1:epochs, test_errors, 1:epochs, lrn_rates);
 endfunction

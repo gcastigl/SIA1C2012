cc21b6787c0b1ace2288d7545762dc160bc9da40
martin <martin@martin-Inspiron-1545.(none)>
2012-04-16 21:19:16 -0300

    fixed horrible bug

--- TPE_pre2/src/octave/get_deltas.m
+++ TPE_pre2/src/octave/get_deltas.m
@@ -13,20 +13,22 @@ function deltas = get_deltas(net, expected)
 		%dont bother propagating; is a simple perceptron
 		return;
 	endif
-	for l = (layers-2):1
+	l = layers - 2;
+	while(l != 0)
 		%first calculate g'(h). Notice that in some trans functions this does not apply (i.e.: deriv = 1)
-		for i = 1:size(net.values{l+1},2)
+		for i = 1:net.arch(l+1)
 			deriv = 1;
 			if( net.trans_type == 3 && i != size(net.values{l+1},2))
 					deriv = deriv * net.beta * ( 1 - net.values{l+1}(i) * net.values{l+1}(i));
 			endif
 			%now calculate sumnation of weights * deltas of the previous layer to update current layer.
 			acum = 0;
-			for j = 1:size(net.values{l+2},2)
+			for j = 1:net.arch(l+2)
 				acum += deltas{l+1}(j) * net.weights{l+1}(i,j);
 			end
 			deltas{l}(i) = deriv * acum;
 		end
+	l -=1;
 	end
 endfunction
 

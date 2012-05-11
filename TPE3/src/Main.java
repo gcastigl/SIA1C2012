4d2f6453857782ae3129a80e7b1bb07469cee9fe
Gonzalo Castiglione <gcastigl@gmail.com>
2012-05-10 22:17:54 -0300

    improved main. It is now easier to understand

--- TPE3/src/Main.java
+++ TPE3/src/Main.java
@@ -2,36 +2,60 @@ import hopfield.HopfieldNet;
 
 import java.util.Arrays;
 
-import utils.MatrixUtils;
 import utils.ImageUtils;
-
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		int TOTAL_PATTERNS = 2;
-		int[][] patterns = new int[TOTAL_PATTERNS][];
+		int[][] patterns = new int[2][];
+		int N = getDummyPatterns(patterns);
+
+		HopfieldNet net = new HopfieldNet(N);
+		net.storePatterns(patterns);
+		
+		// For debugging purposes
+//		System.out.println("Pesos: ");
+//		MatrixUtils.print(net.getWeights());
+		
+		// Set here what you want the net to recongnize
+//		int[] recognize = ImageUtils.loadBlackAndWhiteImage("./TPE3/resources/line1.png");
+		int[] recognize = new int[] {1, -1, 1};
+		net.initialize(recognize);
+		int[] ans = net.iterateUntilConvergence();
+		
+		System.out.println("Patron devuelto");
+		System.out.println(Arrays.toString(ans));
+		int patternIndex = getMatchingPatternIndex(patterns, ans);
+		if (patternIndex != -1) {
+			System.out.println("El patron introducido se parece al " + patternIndex);
+		} else {
+			System.out.println("El array devuelto no matchea con ningun patron");
+		}
+	}
+	
+	private static int getMatchingPatternIndex(int[][] patterns, int[] vec) {
+		int index = 0;
+		for (int[] pattern: patterns) {
+			if (Arrays.equals(pattern, vec)) {
+				return index;
+			}
+			index++;
+		}
+		return -1;
+	}
+	
+	private static int getImagePatterns(int[][] patterns) {
 		patterns[0] = ImageUtils.loadBlackAndWhiteImage("./TPE3/resources/line1.png");
 		patterns[1] = ImageUtils.loadBlackAndWhiteImage("./TPE3/resources/line2.png");
 //		patterns[2] = ImageUtils.loadBlackAndWhiteImage("./TPE3/resources/line3.png");
 //		patterns[3] = ImageUtils.loadBlackAndWhiteImage("./TPE3/resources/line4.png");
-		int N = (int) Math.sqrt(patterns[0].length);
-				
-		HopfieldNet net = new HopfieldNet(N);
-		net.storePatterns(patterns);
-		
-		System.out.println("Pesos: ");
-		MatrixUtils.print(net.getWeights());
-		
-		int[] recognize =  ImageUtils.loadBlackAndWhiteImage("./TPE3/resources/line1.png");
-		net.initialize(recognize);
-		
-		System.out.println("Estados iniciales");
-		System.out.println(Arrays.toString(net.getStates()));
-		
-		int[] ans = net.iterateUntilConvergence();
-		System.out.println("Patron devuelto");
-		System.out.println(Arrays.toString(ans));
+		return (int) Math.sqrt(patterns[0].length);	// should always be 64
+	}
+	
+	private static int getDummyPatterns(int[][] patterns) {
+		patterns[0] = new int[] {1, -1, 1};
+		patterns[1] = new int[] {-1, -1, 1};
+		return patterns[0].length;
 	}
 }

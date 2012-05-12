ddc9454ed06a58c343f3097bc1351f5dd86b4d4b
Gonzalo Castiglione <gcastigl@gmail.com>
2012-05-11 21:50:54 -0300

    No estaba bien pensada la logica de fin de iteraciones para la red asincronica

--- TPE3/src/hopfield/AsynchHopfieldNet.java
+++ TPE3/src/hopfield/AsynchHopfieldNet.java
@@ -10,17 +10,28 @@ public class AsynchHopfieldNet extends HopfieldNet {
 
 	@Override
 	public int[] iterateUntilConvergence() {
-		int[] prevStates = states.clone();
+		int maxIterationsWithoutChanges = 10;
+		int iterationsWithoutChanges = 0;
 		do {
 			int index = (int) (Math.random() * getNumNeurons());
-			int state = states[index];
-			float h = 0;
-			for (int i = 0; i < getNumNeurons(); i++) {
-				h += weights[index][i] * states[i];
+			int prevState = states[index];
+			updateUnit(index);
+			if (states[index] == prevState) {
+				iterationsWithoutChanges++;
+			} else {
+				iterationsWithoutChanges = 0;
 			}
-			states[index] = sgn(h, state);
-		} while (!Arrays.equals(prevStates, states));
-		return prevStates;
+		// Repetir la iteración hasta que el vector de estados permanezca sin cambiar
+		} while (iterationsWithoutChanges < maxIterationsWithoutChanges);
+		return states;
+	}
+	
+	private void updateUnit(int index) {
+		float h = 0;
+		for (int i = 0; i < getNumNeurons(); i++) {
+			h += weights[index][i] * states[i];
+		}
+		states[index] = sgn(h, states[index]);
 	}
 
 	private int sgn(float h, int prevState) {

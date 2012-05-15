de1821e0d3c494c3d4e7cdfe55cc5e80839dc455
Gonzalo Castiglione <gcastigl@gmail.com>
2012-05-14 21:25:20 -0300

    agrego codigo para modularizar un poco los tests

--- TPE3/src/hopfield/test/AsynchHopfieldTest.java
+++ TPE3/src/hopfield/test/AsynchHopfieldTest.java
@@ -13,39 +13,38 @@ public class AsynchHopfieldTest {
 
 	@Test
 	public void reconocerPatron() {
-		String[] patternName = {"h.png", "line1.png", "line2.png", "line3.png", "line4.png"};
-		int totalPatterns = patternName.length;
-		int[][] patterns = new int[totalPatterns][];
-		for (int i = 0; i < totalPatterns; i++) {
-			patterns[i] = Config.getImageAsState(patternName[i]);
-		}
-		
-		int N = patterns[0].length;
-		HopfieldNet net = new AsynchHopfieldNet(N);
-		net.storePatterns(patterns);
+		String[] patternNames = {"h.png", "line1.png", "line2.png", "line3.png", "line4.png"};
 		int[] recognize = Config.getImageAsState("line1.png");
-		net.initialize(recognize);
-		int[] ans = net.iterateUntilConvergence();
+		int[] ans = testNet(getPatterns(patternNames), recognize);
+		Assert.assertTrue(Arrays.equals(recognize, ans));
+	}
 		Assert.assertTrue(Arrays.equals(recognize, ans));
 	}
 	
 	@Test
 	public void noReconocerPatron() {
-		String[] patternName = {"h.png", "line1.png", "line2.png", "line3.png", "line4.png"};
-		int totalPatterns = patternName.length;
-		int[][] patterns = new int[totalPatterns][];
-		for (int i = 0; i < totalPatterns; i++) {
-			patterns[i] = Config.getImageAsState(patternName[i]);
-		}
-		
-		int N = patterns[0].length;
-		HopfieldNet net = new AsynchHopfieldNet(N);
-		net.storePatterns(patterns);
+		String[] patternNames = {"h.png", "line1.png", "line2.png", "line3.png", "line4.png"};
 		int[] recognize = Config.getImageAsState("pacman.png");
-		net.initialize(recognize);
-		int[] ans = net.iterateUntilConvergence();
+		int[] ans = testNet(getPatterns(patternNames), recognize);
 		// FIXME: Que se supone que devuelve la red cuando se le da 
 		// un patron que nada que ver con los que tiene memorizados??
 		Assert.assertFalse(Arrays.equals(recognize, ans));
+	}
+
+	private int[][] getPatterns(String[] patternNames) {
+		int totalPatterns = patternNames.length;
+		int[][] patterns = new int[totalPatterns][];
+		for (int i = 0; i < totalPatterns; i++) {
+			patterns[i] = Config.getImageAsState(patternNames[i]);
+		}
+		return patterns;
+	}
+	
+	private int[] testNet(int[][] patterns, int[] recognize) {
+		int N = patterns[0].length;
+		HopfieldNet net = new AsynchHopfieldNet(N);
+		net.storePatterns(patterns);
+		net.initialize(recognize);
+		return net.iterateUntilConvergence();
 	}
 }

c606afede778bc51d3ab45ab7637a12a23843dd9
Gonzalo Castiglione <gcastigl@gmail.com>
2012-06-02 17:20:08 -0300

    agrego metodo basico de cruze de individuos seleccionados

--- TPE4/src/geneticalgorithm/crossover/CrossoverMethod.java
+++ TPE4/src/geneticalgorithm/crossover/CrossoverMethod.java
@@ -14,7 +14,27 @@ public abstract class CrossoverMethod {
 	 * de un tamaño determinado.
 	 * <pre>
 	 */
-	public abstract Chromosome[] cross(Configuration config, int[] selected);
+	public Chromosome[] cross(Configuration config, int[] selected) {
+		Chromosome[] population = config.population;
+		Chromosome[] newIndividuals = new Chromosome[selected.length]; 
+		boolean oddSelection = selected.length % 2 == 1;
+		int maxLen = selected.length;
+		if (oddSelection) {
+			// Si hay un numero impar, cruzo el ultimo con el primero
+			// y luego todas las parejas desde 0 hasta N - 1 (par)
+			maxLen--;
+			Chromosome[] childs = cross(
+				population[selected[0]], population[selected[maxLen]]);
+			newIndividuals[maxLen] = childs[0];
+		}
+		for (int i = 0; i < maxLen; i += 2) {
+			Chromosome c1 = population[selected[i]];
+			Chromosome c2 = population[selected[i + 1]];
+			Chromosome[] childs = cross(c1, c2);
+			newIndividuals[i] = childs[0];
+			newIndividuals[i + 1] = childs[1];
+		}
+		return newIndividuals;
 	}
 
 	/**

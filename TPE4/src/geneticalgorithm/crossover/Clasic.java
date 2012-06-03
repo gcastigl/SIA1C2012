1b57885e7f369983e8bc87ae4f565e4f6f47a742
Gonzalo Castiglione <gcastigl@gmail.com>
2012-06-02 21:19:18 -0300

    implemento Classic crossover method

--- TPE4/src/geneticalgorithm/crossover/Clasic.java
+++ TPE4/src/geneticalgorithm/crossover/Clasic.java
@@ -1,5 +1,6 @@
 package geneticalgorithm.crossover;
 
+import neuronalnetwork.NetSerializer;
 import geneticalgorithm.Chromosome;
 import geneticalgorithm.Configuration;
 
@@ -16,6 +17,15 @@ public class Clasic extends CrossoverMethod {
 	@Override
 	protected Chromosome[] cross(Chromosome c1, Chromosome c2) {
 		Chromosome[] childs = new Chromosome[2];
+		int totalConnections = NetSerializer.totalConnections(c1.getLayers());
+		int p = (int) (Math.random() * totalConnections);
+		NetSerializer.toArray(c1.getLayers(), netAsArray, 0, p);
+		NetSerializer.toArray(c2.getLayers(), netAsArray, p + 1);
+		childs[0] = NetSerializer.fromArray(config.netConfig.structure, netAsArray);
+		NetSerializer.toArray(c2.getLayers(), netAsArray, 0, p);
+		NetSerializer.toArray(c1.getLayers(), netAsArray, p + 1);
+		childs[1] = NetSerializer.fromArray(config.netConfig.structure, netAsArray);
+		
 		return childs;
 	}
 	

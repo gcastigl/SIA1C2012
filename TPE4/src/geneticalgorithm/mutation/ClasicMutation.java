169e7692dc038de72f7d54bf87084ccdd87b5b01
Gonzalo Castiglione <gcastigl@gmail.com>
2012-06-05 00:09:32 -0300

    implemento metodo de mutacion clasico

--- TPE4/src/geneticalgorithm/mutation/ClasicMutation.java
+++ TPE4/src/geneticalgorithm/mutation/ClasicMutation.java
@@ -2,15 +2,28 @@ package geneticalgorithm.mutation;
 
 import geneticalgorithm.Chromosome;
 import geneticalgorithm.Configuration;
+import neuronalnetwork.NetSerializer;
 
 public class ClasicMutation extends MutationMethod {
 
 	public ClasicMutation(Configuration config) {
 		super(config);
+		if (config.p_mutate <= 0.01) {
+			System.out.println("Warning: mutation value is very low: " + config.p_mutate);
+		}
 	}
 
 	@Override
-	public void mutate(Chromosome childs) {
-		super.mutate(childs);
+	public void mutate(Chromosome c) {
+		super.mutate(c);
+		if (config.p_mutate > Math.random()) {
+			int randIndex = (int) (Math.random() * NetSerializer.totalConnections(c.getLayers()));
+			NetSerializer.toArray(c.getLayers(), netAsArray);
+			netAsArray[randIndex] = getMutatation(netAsArray[randIndex]);
+		}
+	}
+	
+	private float getMutatation(float value) {
+		return value * 0.9f;
 	}
 }

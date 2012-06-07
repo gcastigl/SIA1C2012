5724665498c11331b1ccfe8ceaf1ddbce40a931a
Gonzalo Castiglione <gcastigl@gmail.com>
2012-06-07 19:53:32 -0300

    El tp pide que el cruce sea con cierta probabilidad, no siempre.

--- TPE4/src/geneticalgorithm/crossover/CrossoverMethod.java
+++ TPE4/src/geneticalgorithm/crossover/CrossoverMethod.java
@@ -24,7 +24,6 @@ public abstract class CrossoverMethod extends NetModificator {
 	 * <pre>
 	 */
 	public Chromosome[] cross(int[] selected) {
-		Chromosome[] population = config.population;
 		Chromosome[] newIndividuals = new Chromosome[selected.length]; 
 		boolean oddSelection = selected.length % 2 == 1;
 		int maxLen = selected.length;
@@ -32,19 +31,36 @@ public abstract class CrossoverMethod extends NetModificator {
 			// Si hay un numero impar, cruzo el ultimo con el primero
 			// y luego todas las parejas desde 0 hasta N - 1 (par)
 			maxLen--;
-			cross(population[selected[0]], population[selected[maxLen]]);
-			newIndividuals[maxLen] = childs[0];
+			cross(newIndividuals, maxLen, selected[0], selected[maxLen], 1);
 		}
 		for (int i = 0; i < maxLen; i += 2) {
-			Chromosome c1 = population[selected[i]];
-			Chromosome c2 = population[selected[i + 1]];
-			cross(c1, c2);
-			newIndividuals[i] = childs[0];
-			newIndividuals[i + 1] = childs[1];
+			cross(newIndividuals, i, selected[i], selected[i + 1], 2);
 		}
 		return newIndividuals;
 	}
 
+	/**
+	 * Cruza los chromosomas s1 con s2 con una probabilidad cp. Si el cruce no se produce, se agregan
+	 * ambos chormosomas intactos a la nueva generacion.
+	 */
+	private void cross(Chromosome[] newIndividuals, int index, int s1, int s2, int childsToCreate) {
+		Chromosome[] population = config.population;
+		Chromosome c1, c2;
+		if (config.cp > Math.random()) {
+			// Make a crossover only with probability cp
+			cross(population[s1], population[s2]);
+			c1 = childs[0];
+			c2 = childs[1];
+		} else {
+			// otherwise leave Chromosomes without change
+			c1 = population[s1];
+			c2 = population[s2];
+		}
+		newIndividuals[index] = c1;
+		if (childsToCreate == 2) {
+			newIndividuals[index + 1] = c2;
+		}
+	}
 
 	/**
 	 * Cruza c1 con c2 y devuelve un vector con los dos hijos generados 

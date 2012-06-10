b1793598b0245acd26cebbcc916970dfbc2b74e8
Gonzalo Castiglione <gcastigl@gmail.com>
2012-06-10 10:53:32 -0300

    code style changes

--- TPE4/src/geneticalgorithm/selector/UniversalSelector.java
+++ TPE4/src/geneticalgorithm/selector/UniversalSelector.java
@@ -3,7 +3,7 @@ package geneticalgorithm.selector;
 import geneticalgorithm.Chromosome;
 import geneticalgorithm.Configuration;
 
-public class UniversalSelector extends CandidateSelector{
+public class UniversalSelector extends CandidateSelector {
 
 	public UniversalSelector(Configuration config) {
 		super(config);
@@ -11,38 +11,33 @@ public class UniversalSelector extends CandidateSelector{
 
 	@Override
 	public int[] select() {
-		
-		int k = (int)(config.G * config.N);
+		int k = (int) (config.G * config.N);
 		return selectBestK(k);
-		
 	}
-	
-	private int[] selectBestK( int k){
+
+	private int[] selectBestK(int k) {
 		double F = 0;
-		for(Chromosome c: config.population){
+		for (Chromosome c : config.population) {
 			F += c.getFitness();
 		}
-		
 		int[] selected = new int[k];
 		double[] cumProb = new double[config.population.length];
-		for ( int i = 0 ; i < config.population.length ; i ++){
-			cumProb[i] = config.population[i].getFitness()/F;
-			if( i != 0 )
-				cumProb[i] += cumProb[i-1];
+		for (int i = 0; i < config.population.length; i++) {
+			cumProb[i] = config.population[i].getFitness() / F;
+			if (i != 0)
+				cumProb[i] += cumProb[i - 1];
 		}
 		double g = Math.random() * F;
-		for( int i = 0 ; i < k ; i ++){
+		for (int i = 0; i < k; i++) {
 			boolean chosen = false;
-				double r = (g+i)/(double)k;
-				for( int j = 0 ; j < config.population.length && !chosen; j++){
-					if(r < cumProb[j]){
-						selected[i] = j;
-						chosen = true;
-					}
+			double r = (g + i) / (double) k;
+			for (int j = 0; j < config.population.length && !chosen; j++) {
+				if (r < cumProb[j]) {
+					selected[i] = j;
+					chosen = true;
 				}
 			}
-			
-		
+		}
 		return selected;
 	}
 
@@ -50,7 +45,6 @@ public class UniversalSelector extends CandidateSelector{
 	public void replace(Chromosome[] childs) {
 		int[] selected = selectBestK(config.N - childs.length);
 		createNewPopulation(selected, childs);
-		
 	}
 
 }

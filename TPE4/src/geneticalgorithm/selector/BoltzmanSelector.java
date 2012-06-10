b3f8f958833afa4c6f90a08819facda96041d6e5
Gonzalo Castiglione <gcastigl@gmail.com>
2012-06-10 17:37:00 -0300

    code style changes

--- TPE4/src/geneticalgorithm/selector/BoltzmanSelector.java
+++ TPE4/src/geneticalgorithm/selector/BoltzmanSelector.java
@@ -3,12 +3,12 @@ package geneticalgorithm.selector;
 import geneticalgorithm.Chromosome;
 import geneticalgorithm.Configuration;
 
-public class BoltzmanSelector extends CandidateSelector{
-	
+public class BoltzmanSelector extends CandidateSelector {
+
 	private final double MAX_T = 10000;
 	private final double MIN_T = 1000;
 	private final double DEC_RATE = 5;
-	
+
 	public BoltzmanSelector(Configuration config) {
 		super(config);
 	}
@@ -21,56 +21,54 @@ public class BoltzmanSelector extends CandidateSelector{
 
 	@Override
 	public int[] select() {
-		
-		int k = (int)(config.G * config.N);
+		int k = (int) (config.G * config.N);
 		return selectBestK(k);
-		
 	}
-	
-	private int[] selectBestK( int k){
+
+	@Override
+	protected int[] selectBestK(int k) {
 		double T = getTemperature(config.elapsedGen);
 		double avg = 0;
-		for(Chromosome c: config.population){
-			avg += Math.exp(c.getFitness()/T);
+		for (Chromosome c : config.population) {
+			avg += Math.exp(c.getFitness() / T);
 		}
-		
 		int[] selected = new int[k];
 		double[] cumProb = new double[config.population.length];
 		boolean used[] = new boolean[config.population.length];
-		for ( int i = 0 ; i < config.population.length ; i ++){
-			cumProb[i] = Math.exp(config.population[i].getFitness()/T)/avg;
+		for (int i = 0; i < config.population.length; i++) {
+			cumProb[i] = Math.exp(config.population[i].getFitness() / T) / avg;
 			used[i] = false;
-			if( i != 0 )
-				cumProb[i] += cumProb[i-1];
+			if (i != 0)
+				cumProb[i] += cumProb[i - 1];
 		}
-		for( int i = 0 ; i < k ; i ++){
+		for (int i = 0; i < k; i++) {
 			boolean chosen = false;
-			while(!chosen){
+			while (!chosen) {
 				chosen = false;
 				double r = Math.random();
 				boolean found = false;
-				for( int j = 0 ; j < config.population.length && !chosen && !found ; j++){
-					if(r < cumProb[j]){
-						if( used[j]){
+				for (int j = 0; j < config.population.length && !chosen
+						&& !found; j++) {
+					if (r < cumProb[j]) {
+						if (used[j]) {
 							found = true;
-						}
-						else{
+						} else {
 							chosen = true;
 							selected[i] = j;
 							used[j] = true;
 						}
-						
+
 					}
 				}
 			}
-			
+
 		}
 		return selected;
 	}
-	
-	private double getTemperature(int generations){
+
+	private double getTemperature(int generations) {
 		double ret = MAX_T - generations * DEC_RATE;
-		if( ret < MIN_T)
+		if (ret < MIN_T)
 			return MIN_T;
 		return ret;
 	}
